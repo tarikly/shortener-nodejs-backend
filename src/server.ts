a9928e83127cab22562a9da12d193b9908016f1c
tarikly <tarikly+github@gmail.com>
2022-08-15 20:20:03 -0300

    fix service port

--- src/server.ts
+++ src/server.ts
@@ -3,5 +3,5 @@ import database from './database';
 
 database.sync();
 
-app.listen(process.env.PORT);
+app.listen(`${process.env.PORT}`);
 console.log(`Server is listening on port ${process.env.PORT}`);
