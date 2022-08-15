34d7189817928d71c4e20fb9188e4f19fb4dff9f
tarikly <tarikly+github@gmail.com>
2022-08-15 18:38:36 -0300

    add db conf

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {dialect: 'postgres'});
+const sequelize = new Sequelize(`${process.env.DATABASE_URL}`,  { dialect: pg } );
 
 export default sequelize;
