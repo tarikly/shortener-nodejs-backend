20ed0472816ea0985a9300d03ee490350b988f4f
tarikly <tarikly+github@gmail.com>
2022-08-15 19:20:58 -0300

    fix db conf

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize( `${process.env.DATABASE_URL}`, {dialect: 'postgres'});
+// const sequelize = new Sequelize( `${process.env.DATABASE_URL}`, {dialect: 'postgres'});
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {dialect: 'postgres'},{
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     }
-//   }
-// );
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true
+    }
+});
 
 export default sequelize;
