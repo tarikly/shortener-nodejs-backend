9abbcf5ee08d698d4b61d086684a2afe1987b190
Tárikly Távora <tarikly+github@gmail.com>
2022-08-29 11:29:45 -0300

    set SSL on database conn

--- src/database.ts
+++ src/database.ts
@@ -5,12 +5,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(`${process.env.DATABASE_URL}`, {
     dialect: 'postgres',
     protocol: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
+    dialectOptions: {},
 });
 
 export default sequelize;
